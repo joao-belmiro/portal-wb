53d86dbb9bc2a94a749c6e8b0707b765dd1d826f
joão carlos belmiro gonçalves <joao_belmiro@hotmail.com>
2023-10-06 17:10:18 -0300

    novo estilo de lista

--- components/Proposit.vue
+++ components/Proposit.vue
@@ -2,19 +2,19 @@
   <div class="p-container">
     <h2>Propósito</h2>
     <div class="p-list">
-      <ul>
-        <li>Alcance global</li>
-        <li>Marketing eficiente</li>
-        <li>Construção de marca</li>
-        <li>Atender necessidades únicas</li>
-      </ul>
+      <div class="column">
+        <div class="item-list">Alcance global</div>
+        <div class="item-list">Marketing eficiente</div>
+        <div class="item-list">Construção de marca</div>
+        <div class="item-list">Atender necessidades únicas</div>
+      </div>
 
-      <ul>
-        <li>Compartilhar conhecimento</li>
-        <li>Engajar audiência</li>
-        <li>Monetização potencial</li>
-        <li>Automatizar processos</li>
-      </ul>
+      <div class="column">
+        <div class="item-list">Compartilhar conhecimento</div>
+        <div class="item-list">Engajar audiência</div>
+        <div class="item-list">Monetização potencial</div>
+        <div class="item-list">Automatizar processos</div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,22 +38,42 @@ export default {};
   .p-list {
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
-    width: 100%;
+    margin-left: 24px;
+    margin-top: 32px;
+    gap: 5%;
+    width: 95%;
     @media (max-width: 700px) {
+      gap: 0;
+      width: 100%;
       flex-direction: column;
       align-items: flex-start;
     }
-    ul {
-      list-style: url("@/assets/images/check-circle.svg");
-      li {
+    .column {
+      width: 95%;
+      .item-list {
+        box-shadow: 0.25rem 0.25rem 0.6rem rgba(0, 0, 0, 0.05),
+          0 0.5rem 1.125rem rgba(75, 0, 0, 0.05);
+        position: relative;
         text-align: start;
         font-size: 20px;
-        margin-bottom: 12px;
+        margin-bottom: 24px;
+        padding: 12px;
+        border-radius: 32px;
+        border: 1px solid #ddd;
         @media (max-width: 700px) {
           margin-left: 30px;
-          font-size: 16px;
+          font-size: 26px;
+        }
+        &::before {
+          padding: 4px 8px;
+          background: #add8e6;
+          border-radius: 1rem 1rem 0 1rem;
+          position: absolute;
+          left: -24px;
+          top: -20px;
+          content: url("@/assets/images/check-circle.svg");
         }
       }
     }
